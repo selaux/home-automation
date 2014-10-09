45fe07a4696d1a26e32a743620254ab7d873f5d6
Stefan Lau <github@stefanlau.com>
2014-10-09 12:45:18 +0200

    Comply with PEP8 standards.

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -1,3 +1,5 @@
+"""Should eventually become the gateway between rabbitmq and the sensors/actuators listening on rf24 radio"""
+
 from nrf24 import NRF24
 from Crypto.Cipher import AES
 import RPi.GPIO as GPIO
@@ -6,18 +8,23 @@ import struct
 import random
 import asyncio
 
-KEY = bytes([25, 123, 90, 174, 198, 145, 40, 33, 98, 90, 90, 111, 78, 65, 184, 188 ])
+KEY = bytes([25, 123, 90, 174, 198, 145, 40, 33, 98, 90, 90, 111, 78, 65, 184, 188])
 SERVER_ADDRESS = [0xf0, 0xf0, 0xf0, 0xf0, 0xe1]
 
-def initializeGPIO():
+
+def initialize_gpio():
+    """Initialize GPIO (handles pin numbering and cleanup)"""
     GPIO.setmode(GPIO.BCM)
 
     @atexit.register
-    def cleanupGPIO():
+    def dummy_cleanup_gpio():
+        """Clean up GPIO pins"""
         print("Cleaning up GPIO before exit")
         GPIO.cleanup()
 
-def initializeRadio():
+
+def initialize_radio():
+    """Setup the radio module"""
     radio = NRF24()
     radio.begin(0, 0, 25, 24)
 
@@ -36,71 +43,85 @@ def initializeRadio():
 
     return radio
 
-def decryptPacket(packet):
+
+def decrypt_packet(packet):
+    """Decrypt packet for transmission over rf24"""
     cipher = AES.new(KEY, AES.MODE_ECB)
 
-    encryptedPart1 = bytes(packet[0:16])
-    encryptedPart2 = bytes(packet[16:32])
+    encrypted_part1 = bytes(packet[0:16])
+    encrypted_part2 = bytes(packet[16:32])
 
-    intermediatePart2 = cipher.decrypt(encryptedPart2)
-    decrypedPart2 = bytes([e ^ i for e, i in zip(encryptedPart1, intermediatePart2)])
-    decrypedPart1 = cipher.decrypt(encryptedPart1)
+    intermediate_part2 = cipher.decrypt(encrypted_part2)
+    decrypted_part2 = bytes([e ^ i for e, i in zip(encrypted_part1, intermediate_part2)])
+    decrypted_part1 = cipher.decrypt(encrypted_part1)
 
-    return decrypedPart1 + decrypedPart2
+    return decrypted_part1 + decrypted_part2
 
-def encryptPacket(packet):
+
+def encrypt_packet(packet):
+    """Encrypt packet for transmission over rf24"""
     cipher = AES.new(KEY, AES.MODE_ECB)
 
-    decryptedPart1 = bytes(packet[0:16])
-    decryptedPart2 = bytes(packet[16:32])
+    decrypted_part1 = bytes(packet[0:16])
+    decrypted_part2 = bytes(packet[16:32])
 
-    encryptedPart1 = cipher.encrypt(decryptedPart1)
-    intermediatePart2 = bytes([e ^ i for e, i in zip(encryptedPart1, decryptedPart2)])
-    encryptedPart2 = cipher.encrypt(intermediatePart2)
+    encrypted_part1 = cipher.encrypt(decrypted_part1)
+    intermediate_part2 = bytes([e ^ i for e, i in zip(encrypted_part1, decrypted_part2)])
+    encrypted_part2 = cipher.encrypt(intermediate_part2)
 
-    return encryptedPart1 + encryptedPart2
+    return encrypted_part1 + encrypted_part2
 
-def sendPacket(radio, address, packetId, payload):
+
+def send_packet(radio, address, packet_id, payload):
+    """Send packet to an address"""
     payload_size = len(payload)
-    packed = struct.pack('BBBB28s', 1, 0, packetId, payload_size, payload)
+    packed = struct.pack('BBBB28s', 1, 0, packet_id, payload_size, payload)
 
     radio.stopListening()
     radio.openReadingPipe(1, SERVER_ADDRESS)
     radio.openWritingPipe(address)
-    success = radio.write(encryptPacket(bytes(packed)))
+    success = radio.write(encrypt_packet(bytes(packed)))
     radio.startListening()
     if not success:
         print("Failed sending packet!")
 
+
 @asyncio.coroutine
 def handle(radio, packet):
-    counter, clientId, messageType, payloadSize = struct.unpack('BBBB', packet[:4])
-    print("Recieving message type {0} with counter {1}".format(messageType, counter))
-    if messageType == 0:
+    """Handle decrypted incoming packets from the radio"""
+    counter, client_id, message_type = struct.unpack('BBB', packet[:3])
+    print("Recieving message type {0} with counter {1}".format(message_type, counter))
+    if message_type == 0:
         address = list(struct.unpack('<BBBBBBBBBBBB20s', packet)[4:12][::-1][3:8])
-        clientId = random.randint(1, 254)
-        print("Register {0} as {1}".format(bytes(address), clientId))
+        client_id = random.randint(1, 254)
+        print("Register {0} as {1}".format(bytes(address), client_id))
         yield from asyncio.sleep(0.02)
-        sendPacket(radio, address, 1, bytes([ clientId ]))
+        send_packet(radio, address, 1, bytes([client_id]))
+
 
 @asyncio.coroutine
 def run():
-    initializeGPIO()
-    radio = initializeRadio()
+    """Poll for radio messages, decrypt them and pass them to the handler"""
+    initialize_gpio()
+    radio = initialize_radio()
 
     while True:
         while not radio.available():
             yield from asyncio.sleep(0.04)
 
-        encryptedPacket = []
-        radio.read(encryptedPacket, 32)
-        message = decryptPacket(encryptedPacket)
+        encrypted_packet = []
+        radio.read(encrypted_packet, 32)
+        message = decrypt_packet(encrypted_packet)
         asyncio.async(handle(radio, bytes(message)))
 
-if __name__ == "__main__":
+def main():
+    """Runs the gateway"""
     loop = asyncio.get_event_loop()
     asyncio.async(run())
     try:
         loop.run_forever()
     finally:
         loop.close()
+
+if __name__ == "__main__":
+    main()
