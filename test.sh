c75b879020cb06395e59add8dfe1f673e826cd94
Stefan Lau <github@stefanlau.com>
2014-10-10 16:42:32 +0200

    Set TEST_ENV in script.

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 pylint --rcfile=.pylintrc gateway/*.py
 pylint --rcfile=test/.pylintrc test/gateway/*.py
-python -m unittest discover -s test/gateway -p 'test_*.py'
+TEST_ENV=1 python -m unittest discover -s test/gateway -p 'test_*.py'
