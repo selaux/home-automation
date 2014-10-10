923e56a1efb14597083a61a1ca10b497c8aec6a7
Stefan Lau <github@stefanlau.com>
2014-10-10 19:28:25 +0200

    Add coverage reporting.

--- test.sh
+++ test.sh
@@ -4,4 +4,5 @@ set -e
 
 pylint --rcfile=.pylintrc gateway/*.py
 pylint --rcfile=test/.pylintrc test/gateway/*.py
-TEST_ENV=1 python -m unittest discover -s test/gateway -p 'test_*.py'
+TEST_ENV=1 coverage run -m unittest discover -s test/gateway -p 'test_*.py'
+coverage report --fail-under 95
