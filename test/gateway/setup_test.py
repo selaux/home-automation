e669ced49c2f836c45906509e7122cdb62d8df0a
Stefan Lau <github@stefanlau.com>
2014-10-10 18:20:18 +0200

    Add test for router.

--- test/gateway/setup_test.py
+++ test/gateway/setup_test.py
@@ -1,7 +1,16 @@
 import sys
+import asyncio
 from os import path
 
 def setup():
     gateway_path = path.realpath(path.abspath(path.join(__file__, '../../../gateway/')))
     if not gateway_path in sys.path:
         sys.path.append(gateway_path)
+
+def async_test(function):
+    def wrapper(*args, **kwargs):
+        coro = asyncio.coroutine(function)
+        future = coro(*args, **kwargs)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(future)
+    return wrapper
---
+++ test/gateway/test_router.py
@@ -0,0 +1,34 @@
+import setup_test
+setup_test.setup()
+
+import unittest
+import router
+import random
+from mock import Mock
+
+class TestRouter(unittest.TestCase):
+    mock_server_id = [1, 2, 3, 4, 5, 6, 7, 8]
+
+    def setUp(self):
+        self.send_packet_stub = Mock()
+        self.server_id_before = router.SERVER_ID
+        router.SERVER_ID = self.mock_server_id
+
+    def tearDown(self):
+        router.SERVER_ID = self.server_id_before
+
+    @setup_test.async_test
+    def test_it_should_handle_a_registration_packet_and_send_back_an_acknowledgement(self):
+        registration_packet_id = 0
+        payload = bytes([1, 2, 3])
+        expected_client_id = random.randint(1, 255)
+        expected_packet_id = 1
+        expected_response = bytes([expected_client_id]) + bytes(self.mock_server_id)
+        router_instance = router.Router(self.send_packet_stub)
+
+        yield from router_instance.handle_packet(expected_client_id, registration_packet_id, payload)
+
+        self.send_packet_stub.assert_called_once_with(expected_client_id, expected_packet_id, expected_response)
+
+
+
